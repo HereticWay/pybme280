ccb841fc86e29d98a056fa20bbac53be6eebde7a
HereticWay <vpleft@gmail.com>
2021-12-12 11:05:07 +0100

    use slots in BME280; check type of values got in BME280's constructor;

--- bme280/bme280.py
+++ bme280/bme280.py
@@ -5,15 +5,36 @@ _CHIP_ID_REG = 0xD0
 
 
 class BME280:
+    __slots__ = (
+        "_i2c_address",
+        "_smbus",
+        "_sensor_config",
+        "_chip_id"
+    )
+
+    _i2c_address: int
+    _smbus: smbus.SMBus
+    _sensor_config: SensorConfig
+    _chip_id: int
+
     def __init__(self,
                  i2c_address: int,
                  i2c_port: int,
-                 sensor_config: SensorConfig = SensorConfig()) -> None:
+                 sensor_config: SensorConfig = SensorConfig()
+                 ) -> None:
+
+        if i2c_address is not int or i2c_address < 0:
+            raise ValueError("i2c_address should be a positive integer!")
+        if i2c_port is not int or i2c_port < 0:
+            raise ValueError("i2c_port should be a positive integer!")
+        if sensor_config is not SensorConfig:
+            raise ValueError("sensor_config should be a SensorConfig object!")
+
         self._i2c_address = i2c_address
         self._smbus = smbus.SMBus(i2c_port)
-        self._chip_id = self._smbus.read_byte_data(self._i2c_address, _CHIP_ID_REG)
         self._sensor_config = sensor_config
 
+        self._chip_id = self._smbus.read_byte_data(self._i2c_address, _CHIP_ID_REG)
         self._init_sensor()
 
     def _init_sensor(self) -> None:
@@ -22,5 +43,5 @@ class BME280:
     def _reset_sensor(self) -> None:
         pass
 
-    def get_chip_id(self) -> int:
+    def chip_id(self) -> int:
         return self._chip_id
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from bme280 import bme280
 
 def main():
     bme = bme280.BME280(i2c_address=0x77, i2c_port=1)
-    print(f'Chip id: {hex(bme.chip_id)}')
+    print(f'Chip id: {hex(bme.chip_id())}')
 
 
 if __name__ == '__main__':
